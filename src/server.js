abdd808d1e884c29867704c3d61f871286d6a6fb
ajeer333 <ajeer333@gmail.com>
2023-03-14 16:47:23 +0100

    commit

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import {
   badRequestHandler,
   notFoundHandler,
@@ -8,8 +9,25 @@ import {
 import blogsRouter from "./api/blogs/index.js";
 const server = express();
 const port = process.env.PORT;
+const whitelist = [process.env.FE_DEV_URL];
+server.use(
+  cors({
+    origin: (currentOrigin, corsNext) => {
+      if (!currentOrigin || whitelist.indexOf(currentOrigin) !== -1) {
+        corsNext(null, true);
+      } else {
+        corsNext(
+          createHttpError(
+            400,
+            `Origin ${currentOrigin} is not in the whitelist!`
+          )
+        );
+      }
+    },
+  })
+);
 server.use(express.json());
-server.use("/blogPosts", blogsRouter);
+server.use("/blogs", blogsRouter);
 server.use(badRequestHandler);
 server.use(notFoundHandler);
 server.use(genericErrorHandler);
